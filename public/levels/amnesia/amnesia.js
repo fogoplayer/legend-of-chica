17d45a7ae2524e394db9391e14a5d293b5933297
Zarin Loosli <fogoplayer@gmail.com>
2018-07-04 22:00:42 +0000

    BBB now comes in after scream

--- public/levels/amnesia/amnesia.js
+++ public/levels/amnesia/amnesia.js
@@ -7,47 +7,46 @@ import bbb from '../../sprites/bbb/bbb.js';
 import tbt from '../../sprites/tbt/tbt.js';
 
 export default class Amnesia extends Phaser.Scene {
-    
+
     /**
      * Constructor for Amnesia
      * @param null
      * @return null
-    **/
+     **/
     constructor() {
         super({
             key: 'Level'
         });
         level.initialize(this);
     }
-    
+
     /**
      * Preload for Amnesia
      * @param null
      * @return null
-    **/
+     **/
     async preload() {
         chica.preload(this);
         bbb.preload(this);
         tbt.preload(this);
         this.load.image('map', './levels/amnesia/amnesia.png');
     }
-    
+
     /**
      * Create for Amnesia
      * @param null
      * @return null
-    **/
+     **/
     async create() {
-        
-        
-        let map = this.add.image(0,0,'map').setOrigin(0,0).setScale(2.4);
-        
+
+
+        let map = this.add.image(0, 0, 'map').setOrigin(0, 0).setScale(2.4);
+
         const _this = this;
         this.sceneWidth = this.sys.game.config.width;
         this.sceneHeight = this.sys.game.config.height;
         chica.createInWorld(this, this.sceneWidth / 3, this.sceneHeight / 2);
-        await Dialogue.dialogueConstructorWithPromise(this, [
-            {
+        await Dialogue.dialogueConstructorWithPromise(this, [{
                 char: 'Chica',
                 text: 'Wooooof…'
             },
@@ -64,8 +63,8 @@ export default class Amnesia extends Phaser.Scene {
                 text: 'Who\'s that?'
             },
         ]);
-        
-        
+
+
         await (() => {
             return new Promise((resolve, reject) => {
                 tbt.createInWorld(this, this.sceneWidth + 50, this.sceneHeight / 2);
@@ -74,7 +73,12 @@ export default class Amnesia extends Phaser.Scene {
                 });
             });
         })();
-        
+
+        await Dialogue.dialogueConstructorWithPromise(this, [{
+            char: 'Tiny Box Tim',
+            text: 'AHHHHHHHHHHHHHHH!!!!!!!!'
+        }, ]);
+
         await (() => {
             return new Promise((resolve, reject) => {
                 bbb.createInWorld(this, this.sceneWidth + 50, this.sceneHeight / 2);
@@ -83,22 +87,17 @@ export default class Amnesia extends Phaser.Scene {
                 });
             });
         })();
-            
-        await Dialogue.dialogueConstructorWithPromise(this, [
-            {
-                char: 'Tiny Box Tim',
-                text: 'AHHHHHHHHHHHHHHH!!!!!!!!'
-            },
-            {
+
+        await Dialogue.dialogueConstructorWithPromise(this, [{
                 char: 'Chica',
                 text: 'Tiny Box Tim? You’re Mark’s little biscuit! I’ll save you!!!'
             },
         ]);
-        
+
         level.changeLevel('Tutorial', this);
     }
-    
-    update(){
+
+    update() {
         chica.updateInWorld(this);
     }
 }
