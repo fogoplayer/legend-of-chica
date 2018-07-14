6f3b80fe410b6f5ec0c205dfc8c82a1ad227c824
Zarin Loosli <fogoplayer@gmail.com>
2018-07-14 10:56:29 -0600

    Change save dialog font to Welbutrin

--- public/levels/level.js
+++ public/levels/level.js
@@ -6,8 +6,8 @@ const level = {
      * @param levelName-The name of the level to be called
      * @param levelType-The type of level (world/battle)
      * @param chars-an array of characters to import for the level
-    **/
-    initialize: async (_this, levelType = undefined) => {
+     **/
+    initialize: async(_this, levelType = undefined) => {
         Phaser.Scene.call(_this, {
             key: 'Level',
             active: true
@@ -15,22 +15,22 @@ const level = {
         
         //Modules that every level uses
         let moduleList = ['../systems/dialogue.js'];
-        
+
         //Modules used for specific level types
-        if(levelType === 'world'){
-            
+        if (levelType === 'world') {
+
         }
-        if(levelType === 'battle'){
+        if (levelType === 'battle') {
             moduleList = moduleList.concat([
                 '../systems/combatHud/combatHud.js',
                 '../systems/combatHud/actionOptions.js',
                 '../systems/combat.js'
             ]);
         }
-        
+
         await system.addModules(moduleList)
     },
-    
+
     /**
      * Loads in dependencies
      * NOT CURRENTLY IN USE
@@ -38,15 +38,15 @@ const level = {
      * @param _this-The current scene
      * @param chars-an array of characters to import for the level
      * @param levelType-The type of level (world/battle)
-    **/
+     **/
     // loadModules: async (_this, chars = [], levelType = undefined) => {
-        
+
     //     //Modules that every level uses
     //     let moduleList = ['../systems/dialogue.js'];
-        
+
     //     //Modules used for specific level types
     //     if(levelType === 'world'){
-            
+
     //     }
     //     if(levelType === 'battle'){
     //         moduleList = moduleList.concat([
@@ -55,43 +55,48 @@ const level = {
     //             '../systems/combat.js'
     //         ]);
     //     }
-        
+
     //     //Character modules
     //     (typeof chars !== 'object') ? chars = [chars] : null;
     //     moduleList = moduleList.concat(chars.map((char) => {
     //         return `../sprites/${ char.toLowerCase() }/${ char.toLowerCase() }.js`;
     //     }));
-        
+
     //     await system.addModules(moduleList)
     // },
-    
+
     /**
      * Changes the current level
      * @param newLevel-the key of the level to be added
      * @param _this-the current scene object
-    **/
-    changeLevel: async (newLevel, _this) => {
-        _this.sound.sounds.forEach(sound => { sound.destroy() });
-        
+     **/
+    changeLevel: async(newLevel, _this) => {
+        //Load welbutrin
+        _this.load.bitmapFont('welbutrin', './assets/font.png', 'assets/font.fnt');
+    
+        _this.sound.sounds.forEach(sound => {
+            sound.destroy()
+        });
+
         await system.addModules([`../levels/${ newLevel.toLowerCase() }/${ newLevel.toLowerCase() }.js`])
         let scenes = _this.scene.manager.scenes
         system.userData.currentLevel = newLevel;
-        _this.add.text(384, 384, 'Progress Saved', {
+        _this.add.bitmapText(384, 384, 'welbutrin', 'Progress Saved', 32/*{
             fontSize: '32px',
             fill: '#ffffff',
             color: '#ffffff',
             backgroundColor: '#000000bb',
-        }).setOrigin(0.5, 0.5);
-        
+        }*/).setOrigin(0.5, 0.5);
+
         setTimeout(() => {
             while (scenes.length > 0) {
-                    _this.scene.manager.remove(scenes[0].scene.key);
-                };
+                _this.scene.manager.remove(scenes[0].scene.key);
+            };
             setTimeout(() => {
                 _this.scene.manager.add(newLevel, window[newLevel]);
             }, 5);
         }, 1000);
-        
+
         system.save(_this);
         console.clear();
     }
